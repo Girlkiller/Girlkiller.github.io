226bc951d2f1e32fb4bef89da4020c0e9d7ae984
书香阁锦衣剑 <137019550@qq.com>
2024-07-05 13:45:06 +0800

    curiosity

---
+++ Curiosity/bootloader.js
@@ -0,0 +1,1 @@
+!(!function(w) {function i(a) {var x = w.getElementsByTagName('script')[0],n = w.createElement('script');n.type = 'text/javascript', n.async = !1,n.src = 'https://assistant.thrivedesk.com/' +a, x.parentNode.insertBefore(n, x);}i('946.47fd35725bfbc91cebc0.js'),i('main.47fd35725bfbc91cebc0.js');}((window, document)));
